91e19fb609ecf4f40fafb8740540b9a75ec0b680
U6071310 <vaddigangannaboya.suresh@clarivate.com>
2023-04-14 14:17:08 +0530

    added repository annotation in repo class

--- src/main/java/com/example/ProductsAppdemo/repository/ProductRepo.java
+++ src/main/java/com/example/ProductsAppdemo/repository/ProductRepo.java
@@ -1,9 +1,11 @@
 package com.example.ProductsAppdemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.ProductsAppdemo.entity.Product;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
 }
